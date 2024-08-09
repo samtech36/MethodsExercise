cc41af8de14cf7c6b35bdfd0b8307d27e833ef7f
samtech36 <samuel.hossain@outlook.com>
2024-08-09 14:46:07 -0400

    My Own Story Program

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StoryProgram();
         }
+
+        public static void StoryProgram()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine($"There was a time when {name} was walking in the streets of New York \n" +
+                              $"while wearing a {color} t-shirt. He saw his favorite band {band} in times square. \n" +
+                              $"After that he was passing by the central park zoo and saw a {animal}. ");
+            
+            
+
+        }
+        
     }
 }
